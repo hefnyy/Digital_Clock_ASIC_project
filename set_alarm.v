611830b8a8a0bc8de1e11335ae2fa2331e559c94
hefnyy <mohammedahefny1111@gmail.com>
2025-12-17 23:37:43 +0200

    modify on set alram

--- set_alarm.v
+++ set_alarm.v
@@ -1,33 +1,69 @@
 module set_alarm (
     input clk, rst, set_alarm_en,
     input mode_button, inc_button,
-    output reg [4:0] o_hours,
-    output reg [5:0] o_minutes,
+    output reg [1:0] o_hours_left,
+    output reg [3:0] o_hours_right,
+    output reg [2:0] o_minutes_left,
+    output reg [3:0] o_minutes_right,
     output ack_flag, 
     output reg on_off_alarm
 );
-    reg [1:0] modes;
+    reg [2:0] modes;
 
     always @(posedge clk or negedge rst) begin
         if (!rst) begin
-            o_hours <= 0;
-            o_minutes <= 0;
+            o_hours_left <= 0;
+            o_hours_right <= 0;
+            o_minutes_left <= 0;
+            o_minutes_right <= 0;
             modes <= 0;
             on_off_alarm <= 0;
-        end else if(set_alarm_en) begin           
-                if (modes == 0) begin
+        end else if(set_alarm_en) begin            
+                if (modes == 0) begin                       // Modify on (h)h:mm
+                    if (mode_button) begin  
+                        modes <= modes + 1; 
+                    end else if (inc_button == 1) begin     
+                        o_hours_left <= o_hours_left + 1;
+                        if (o_hours_left == 2) begin
+                            o_hours_left <= 0;
+                        end
+                    end
+                end else if (modes == 1) begin              // Modify h(h):mm
+                    if (mode_button) begin 
+                        modes <= modes + 1; 
+                    end else if (inc_button == 1) begin     
+                        if (o_hours_left == 2) begin
+                            if (o_hours_right == 3) begin
+                                o_hours_right <= 0;
+                            end else begin
+                                o_hours_right <= o_hours_right + 1;
+                            end
+                        end else begin
+                            o_hours_right <= o_hours_right + 1;
+                            if (o_hours_right == 9) begin
+                                o_hours_right <= 0;
+                            end
+                        end
+                    end
+                end else if (modes == 2) begin              // Modify h(h):(m)m
                     if (mode_button) begin
                         modes <= modes + 1;
                     end else if (inc_button == 1) begin
-                        o_hours <= o_hours + 1;
+                        o_minutes_left <= o_minutes_left + 1;
+                        if (o_hours_left == 5) begin
+                            o_minutes_left <= 0;
+                        end
                     end
-                end else if (modes == 1) begin
+                end else if (modes == 3) begin             // Modify on (h)h:m(m)
                     if (mode_button) begin
                         modes <= modes + 1;
                     end else if (inc_button == 1) begin
-                        o_minutes <= o_minutes + 1;
+                        o_minutes_right <= o_minutes_right + 1;
+                        if (o_hours_right == 9) begin
+                            o_minutes_right <= 0;
+                        end
                     end
-                end else if (modes == 2) begin
+                end else if (modes == 4) begin             // Set Alarm On or Off
                     if (mode_button) begin
                         modes <= modes + 1;
                     end else if (inc_button) begin
@@ -41,7 +77,7 @@ module set_alarm (
         end
     end
 
-    assign ack_flag = (modes == 3);
+    assign ack_flag = (modes == 5);
 
 
 endmodule
--- set_alarm_tb.v
+++ set_alarm_tb.v
@@ -3,13 +3,15 @@
 module set_alarm_tb ();
     reg clk, rst, set_alarm_en;
     reg mode_button, inc_button;
-    wire [4:0] o_hours;
-    wire [5:0] o_minutes;
+    wire [1:0] o_hours_left;
+    wire [3:0] o_hours_right;
+    wire [2:0] o_minutes_left;
+    wire [3:0] o_minutes_right;
     wire ack_flag; 
     wire on_off_alarm;
 
     set_alarm DUT (clk,rst,set_alarm_en,mode_button,inc_button,
-        o_hours,o_minutes,ack_flag,on_off_alarm);
+        o_hours_left,o_hours_right,o_minutes_left,o_minutes_right,ack_flag,on_off_alarm);
 
     initial begin
         clk = 0;
@@ -18,6 +20,7 @@ module set_alarm_tb ();
         end
     end
 
+        integer i;
     initial begin
         rst = 1;
         @(negedge clk);
@@ -33,10 +36,12 @@ module set_alarm_tb ();
         mode_button = 1;
         @(negedge clk);
         mode_button = 0;
-        @(negedge clk);
-        inc_button = 1;
-        @(negedge clk);
-        inc_button = 0;
+        for (i = 0; i<20 ; i = i + 1  ) begin
+            @(negedge clk);
+            inc_button = 1;
+            @(negedge clk);
+            inc_button = 0;
+        end
         @(negedge clk);
         mode_button = 1;
         @(negedge clk);
